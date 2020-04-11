aa2cea337b3dec33658d34a360c52934e9f7b0a7
seema1611 <seemarajpure16@gmail.com>
2020-04-11 18:04:58 +0530

    ADDED: Function to check validation of user email address

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,9 +2,12 @@
 shopt -s extglob
 
 #Pattern for first and last name
-name_Pattern=^[A-Z]{1}[a-z]{3,}$
+name_Pattern="^[A-Z]{1}[a-z]{3,}$"
 
-#Function to check pattren for first name
+#Pattern for email
+email_Pattern="^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}([.][A-Za-z]{2,4}+)?$"
+
+#Function to check name email and password
 function toCheck() {
 
 	pattern=$2
@@ -15,8 +18,15 @@ function toCheck() {
 		echo "Invalid Format"
 	fi
 }
+
+#Enter name for first name
 read -p "Enter the first name: " first_Name
 toCheck $first_Name $name_Pattern
 
+#Enter name for last name
 read -p "Enter the last name: " last_Name
 toCheck $last_Name $name_Pattern
+
+#Enter name for email address
+read -p "Enter the email address: " email_Address
+toCheck $email_Address $email_Pattern
