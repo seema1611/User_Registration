d83ec7a356f2e6c141080e8f00bbb30823ca224a
seema1611 <seemarajpure16@gmail.com>
2020-04-11 19:01:01 +0530

    ADDED: Function to check password validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -11,9 +11,12 @@ email_Pattern="^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{
 mobile_Pattern="^[0-9]{1,3}[ ][0-9]{10}$"
 
 #Pattern for passwords
-first_Password_Pattern="^[A-za-z]{8,}$"               				        #Rule 1: minmum 8 chacter
-second_Password_Pattern="^(?=.*[A-Z])[A-Za-z]{8,}$"         				#Rule 2: Atleast one uppercase chacter
-third_Password_Pattern="^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$"			#Rule 3: Atleast one numeric character
+first_Password_Pattern="^[A-za-z]{8,}$"               				       		#Rule 1: minmum 8 chacter
+second_Password_Pattern="^(?=.*[A-Z])[A-Za-z]{8,}$"         					#Rule 2: Atleast one uppercase chacter
+third_Password_Pattern="^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$"				#Rule 3: Atleast one numeric character
+four_Password_Pattern="^(?=.*[!@#$%])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9!@#$%]{8,}$"		#Rule 4: Exact one special character
+
+final_Password_Pattern="^(?=.*[!@#$%])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9!@#$%]{8,}$"		#All Condition statisfy pattern
 
 #Function to check name email and password
 function toCheck() {
@@ -39,4 +42,4 @@ read -p "Enter the mobile number: " mobile_Number
 toCheck $mobile_Number $mobile_Pattern
 
 read -p "Enter the password : " password_Number
-toCheck $password_Number $third_Password_Pattern
+toCheck $password_Number $final_Password_Pattern
