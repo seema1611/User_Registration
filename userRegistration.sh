475bbf22f0b7ed6ac190e1664429ef6d3f85050b
seema1611 <seemarajpure16@gmail.com>
2020-04-11 18:11:37 +0530

    ADDED: Function to check validation of user mobile number

--- userRegistration.sh
+++ userRegistration.sh
@@ -7,6 +7,9 @@ name_Pattern="^[A-Z]{1}[a-z]{3,}$"
 #Pattern for email
 email_Pattern="^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}([.][A-Za-z]{2,4}+)?$"
 
+#Pattern for mobile number
+mobile_Pattern="^[0-9]{1,3}[ ][0-9]{10}$"
+
 #Function to check name email and password
 function toCheck() {
 
@@ -18,15 +21,14 @@ function toCheck() {
 		echo "Invalid Format"
 	fi
 }
-
-#Enter name for first name
 read -p "Enter the first name: " first_Name
 toCheck $first_Name $name_Pattern
 
-#Enter name for last name
 read -p "Enter the last name: " last_Name
 toCheck $last_Name $name_Pattern
 
-#Enter name for email address
 read -p "Enter the email address: " email_Address
 toCheck $email_Address $email_Pattern
+
+read -p "Enter the mobile number: " mobile_Number
+toCheck $mobile_Number $mobile_Pattern
